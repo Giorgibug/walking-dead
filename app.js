268b1730f04b768d45bb2d3ebd2d08183f583c6b
Giorgibug <121309161+Giorgibug@users.noreply.github.com>
2023-02-26 12:26:16 +0400

    Created first array
    added average

---
+++ app.js
@@ -0,0 +1,40 @@
+const numbers = [
+    34, 
+    31, 
+    8, 
+    7, 
+    11, 
+    49, 
+    3, 
+    4, 
+    36, 
+    46,
+];
+const average = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+
+console.log(average);
+
+// second stage
+
+// const people = [
+//    person1 = {
+//         personName : "keto",
+//         age : 29,
+//     }
+//     person2 = {
+//         personName : "gela" ,
+//         age : 32,
+//     };
+//     person3 = {
+//         personName : "rusa", 
+//         age : 30,
+//     };
+//     person4 = {
+//         personName : "kote",
+//         age : 40,
+//     };
+//     person5 = {
+//         personName : "Maka", 
+//         age : 32,
+//     };
+// ]
